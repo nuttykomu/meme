1c3d3e514f8e3d9dc4d949337111aba4735b67f7
Nuttykomu (ナッティ) <komu@nutty.world>
2018-06-27 21:47:16 -0700

    Generate ImageMagick command

--- source/server/handler/generateMeme.js
+++ source/server/handler/generateMeme.js
@@ -1,11 +1,50 @@
+const Spawn = require('child_process').spawn;
+
 function generateMeme(request, reply) {
-    let payload = request.payload;
+    // -------------------------
+    // request.payload
+    // ├── image
+    // ├── top-text
+    // ├── bottom-text
+    // ├── font-size
+    // ├── line-spacing
+    // └── stroke-width
+    // -------------------------
+    const payload = request.payload;
+    const options = [];
+
+    options.push(...[
+        {'font': 'source/server/font/impact.ttf'},
+        {'pointsize': payload['font-size']},
+        {'interline-spacing': payload['line-spacing']},
+        {'gravity': 'north'},
+        {'annotate': `+0+0 "${payload['top-text']}"`},
+        {'gravity': 'south'},
+        {'annotate': `+0+0 "${payload['bottom-text']}"`},
+        {'fill': 'white'},
+        {'stroke': 'black'},
+        {'strokewidth': payload['stroke-width']}
+    ]);
+
+    console.log(commandBuilder(options, 'graph.png', 'output.png'));
 
     // -----------------------------------------------
     // TODO: Generate the meme from the image here.
     // -----------------------------------------------
 
     return reply.file('graph.png');
+}
+
+function commandBuilder(options, input, output) {
+    var flags = '';
+    options.forEach(flag => {
+        console.log(flag);
+        Object.entries(flag).forEach(([key, value]) => {
+            flags += `-${key} ${value} `;
+        });
+    });
+
+    return `magick convert ${flags} ${input} ${output}`;
 }
 
 module.exports = generateMeme;
