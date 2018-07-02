7e0274167ef11cc618a417edd2a3b9429fb8f78a
Nuttykomu (ナッティ) <komu@nutty.world>
2018-07-02 11:26:59 -0700

    Create meme options form

--- client/source/components/MemeOptions.vue
+++ client/source/components/MemeOptions.vue
@@ -1,8 +1,31 @@
 <template>
     <aside class="column is-3">
-        Meme Options
+        <form>
+            Meme Image:<br>
+            <input type="file"><br>
+            Top Text:<br>
+            <textarea></textarea><br>
+            Bottom Text:<br>
+            <textarea></textarea><br>
+            Font Size:<br>
+            <input type="range" min="1" max="100" value="32" class="slider"><br>
+            Stroke Width:<br>
+            <input type="range" min="0" max="10" value="2" class="slider"><br>
+            Padding:<br>
+            <input type="range" min="0" max="100" value="0" class="slider"><br>
+            Border:
+            <input type="checkbox"><br>
+            <button>Generate Meme</button>
+        </form>
     </aside>
 </template>
 
 <script>
 </script>
+
+<style scoped>
+form {
+    margin: 20px;
+}
+</style>
+
