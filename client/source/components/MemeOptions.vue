6c222bcc671701f76b4bdacd661a160324352194
Nuttykomu (ナッティ) <komu@nutty.world>
2018-07-02 11:49:15 -0700

    Bind form data to Vue component

--- client/source/components/MemeOptions.vue
+++ client/source/components/MemeOptions.vue
@@ -1,26 +1,92 @@
 <template>
     <aside class="column is-3">
-        <form>
+        <form
+            enctype="multipart/form-data"
+            @submit.prevent="generateMeme"
+        >
+
             Meme Image:<br>
-            <input type="file"><br>
+            <input
+                type="file"
+                ref="image"
+                @change="updateImage"
+            ><br>
+
             Top Text:<br>
-            <textarea></textarea><br>
+            <textarea
+                v-model="options['top-text']"
+            ></textarea><br>
+
             Bottom Text:<br>
-            <textarea></textarea><br>
+            <textarea
+                v-model="options['bottom-text']"
+            ></textarea><br>
+
             Font Size:<br>
-            <input type="range" min="1" max="100" value="32" class="slider"><br>
+            <input
+                type="range"
+                min="1"
+                max="100"
+                value="32"
+                class="slider"
+                v-model="options['font-size']"
+            ><br>
+
             Stroke Width:<br>
-            <input type="range" min="0" max="10" value="2" class="slider"><br>
+            <input
+                type="range"
+                min="0"
+                max="10"
+                value="2"
+                class="slider"
+                v-model="options['stroke-width']"
+            ><br>
+
             Padding:<br>
-            <input type="range" min="0" max="100" value="0" class="slider"><br>
+            <input
+                type="range"
+                min="0"
+                max="100"
+                value="0"
+                class="slider"
+                v-model="options.padding"
+            ><br>
+
             Border:
-            <input type="checkbox"><br>
+            <input
+                type="checkbox"
+                v-model="options.border"
+            ><br>
+
             <button>Generate Meme</button>
         </form>
     </aside>
 </template>
 
 <script>
+export default {
+    data () {
+        return {
+            options: {
+                'image': null,
+                'top-text': '',
+                'bottom-text': '',
+                'font-size': 32,
+                'stroke-width': 0,
+                'padding': 0,
+                'border': true
+            }
+        };
+    },
+    methods: {
+        updateImage () {
+            this.options.image = this.$refs.image.files[0];
+        },
+        generateMeme () {
+            console.log(this.options);
+        }
+    }
+};
 </script>
 
 <style scoped>
@@ -28,4 +94,3 @@ form {
     margin: 20px;
 }
 </style>
-
